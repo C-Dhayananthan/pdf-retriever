c9689d4baeba5fd2bcb77fd2622a55e7a0740e3b
C-Dhayananthan <dhaya427@gmail.com>
2024-10-07 00:38:27 +0530

    feat: added context middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import time
 from app.routers.router import router
 from app.utilities import dc_logger 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from app.middlewares.contextmiddleware import RequestContextMiddleware
+
 
 logger = dc_logger.LoggerAdap(dc_logger.get_logger(__name__),{"vectordb":"faiss"})
 
@@ -9,6 +15,29 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Server is On":"Status Healthy"}
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(RequestContextMiddleware)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.info("Process Time: " + str(process_time))
+    return response
 
 subapi = FastAPI()
 subapi.include_router(router)
